2ab63f7027232d19cbd294fc708ba2fe8a3f1c28
SSen <i@ssen.name>
2021-12-05 23:37:26 +0900

    feat: add prettier-ignore to output typescript file

--- cosmwasm-typescript-generator/src/workflow/writeTypeScript.ts
+++ cosmwasm-typescript-generator/src/workflow/writeTypeScript.ts
@@ -5,8 +5,9 @@ export function writeTypeScript(source: SourceFile, outFile: string) {
   const printer = createPrinter();
   const code = [
     `// DO NOT EDIT MANUALLY : THIS FILE IS AUTO CREATED`,
-    `/* eslint-disable @typescript-eslint/no-shadow */`,
-    `//noinspection JSUnusedGlobalSymbols`,
+    `/* eslint-disable */`,
+    `// prettier-ignore`,
+    `// noinspection JSUnusedGlobalSymbols`,
     ``,
     printer.printFile(source).replace(/\/\*/g, '/**'),
   ].join('\n');
